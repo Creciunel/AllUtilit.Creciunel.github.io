2adfce329679e28a247ef2223f6a5fe8942d99d3
Creciunel <cr.catalin@mail.ru>
2022-03-02 16:30:49 +0200

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,8 +1,24 @@
 import App from "./app.js";
 // const App = require("app");
-const btn = document.getElementById("btn");
+// const btn = document.getElementById("btn");
 // btn.innerHTML = "App.app";
 // btn.value = App.apply;
 const MyObject = new App("app");
-console.log(MyObject.name);
-btn.innerHTML = MyObject.name;
+let name;
+
+$("body").css("text-align", "center");
+
+$("#btn").click(function(){
+    fetch('https://animechan.vercel.app/api/random')
+    .then(response => response.json())
+    .then(quote => {
+        name = quote["anime"];
+        $("#container").append("<b>Anime name: "+name+"</b>");
+        console.log(quote)}
+        )
+})
+
+
+
+
+// btn.innerHTML = MyObject.name;
