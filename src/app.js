8e8efdb7c0a422c3a7dc501932494069dcba1cfd
Creciunel <cr.catalin@mail.ru>
2022-03-02 18:58:20 +0200

    feat add image

--- src/app.js
+++ src/app.js
@@ -1,3 +1,40 @@
-export default function App(name = "app") {
+export default function App() {
     this.name = name;
+}
+
+App.prototype.get1 = function(){
+    $("#btn").click(function(){
+            $(".add").html("");
+            fetch('https://animechan.vercel.app/api/random')
+            .then(response => response.json())
+            .then(quote => {
+                let name = quote["anime"];
+                let character = quote["character"];
+                let note = quote["quote"];
+        
+                $(".add").append("<h3>Anime</h3><b>Name: "+name+"</b><br><b>Character: "+character+"</b><br><b>Quote: "+note+"</b><br>");
+                $(".add").css("margin", "2%");
+                console.log(quote)}
+                )
+        })
+}
+
+App.prototype.get2 = function(){
+    $("#btn").click(function(){
+        $(".add").html("");
+        fetch('https://anime-facts-rest-api.herokuapp.com/api/v1')
+        .then(response => response.json())
+        .then(quote => {
+            let chek = quote["success"];
+            if(chek === true){
+                console.log(quote["data"])
+                let flag = Math.floor(Math.random() * 14);
+                let name= quote["data"][flag]["anime_name"];
+                let img= quote["data"][flag]["anime_img"];
+                $(".add").append(`<h3>Anime</h3><b>Name: `+name+`<br><img  class="img1" src="`+img+`" alt="sorry problem with photo">"`);
+                $(".img1").css("width","90%");
+            }
+        })
+           
+    })
 }
--- src/index.js
+++ src/index.js
@@ -1,29 +1,24 @@
 import App from "./app.js";
-// const App = require("app");
-// const btn = document.getElementById("btn");
-// btn.innerHTML = "App.app";
-// btn.value = App.apply;
-const MyObject = new App("app");
-let name;
-let character;
-let note;
+
+const MyObject = new App();
+
 $("body").css("text-align", "center");
 
-$("#btn").click(function(){
-    $(".add").html("");
-    fetch('https://animechan.vercel.app/api/random')
-    .then(response => response.json())
-    .then(quote => {
-        name = quote["anime"];
-        character = quote["character"];
-        note = quote["quote"]
-        $(".add").append("<h3>Anime</h3><b>Name: "+name+"</b><br><b>Character: "+character+"</b><br><b>Quote: "+note+"</b><br>");
-        $(".add").css("margin", "2%");
-        console.log(quote)}
-        )
-})
+ MyObject.get2();
 
+// $("#btn").click(function(){
+//     $(".add").html("");
+//     fetch('https://animechan.vercel.app/api/random')
+//     .then(response => response.json())
+//     .then(quote => {
+//         let name = quote["anime"];
+//         let character = quote["character"];
+//         let note = quote["quote"];
 
+//         $(".add").append("<h3>Anime</h3><b>Name: "+name+"</b><br><b>Character: "+character+"</b><br><b>Quote: "+note+"</b><br>");
+//         $(".add").css("margin", "2%");
+//         console.log(quote)}
+//         )
+// })
 
-
-// btn.innerHTML = MyObject.name;
+// second tasck
