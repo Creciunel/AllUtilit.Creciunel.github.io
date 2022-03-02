ef889677d748b4f3982e0cd2f073d9aaae4e2e61
Creciunel <cr.catalin@mail.ru>
2022-03-02 18:22:57 +0200

    fix a repeat words

--- src/index.js
+++ src/index.js
@@ -5,15 +5,20 @@ import App from "./app.js";
 // btn.value = App.apply;
 const MyObject = new App("app");
 let name;
-
+let character;
+let note;
 $("body").css("text-align", "center");
 
 $("#btn").click(function(){
+    $(".add").html("");
     fetch('https://animechan.vercel.app/api/random')
     .then(response => response.json())
     .then(quote => {
         name = quote["anime"];
-        $("#container").append("<b>Anime name: "+name+"</b>");
+        character = quote["character"];
+        note = quote["quote"]
+        $(".add").append("<h3>Anime</h3><b>Name: "+name+"</b><br><b>Character: "+character+"</b><br><b>Quote: "+note+"</b><br>");
+        $(".add").css("margin", "2%");
         console.log(quote)}
         )
 })
